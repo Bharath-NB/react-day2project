670917bfb848675243d7f5bfb14acf7cbcd5ad11
Bharath-NB <Bharath.s8499@gmail.com>
2024-05-23 11:19:55 +0530

    array list and usage of keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
-// Component: App
 const App = () => {
-  return <h1>Hello, World!</h1>
-}
 
-export default App;
+  const friends = [
+    { id: 1, name: 'Bharath', age: 25 },
+    { id: 2, name: 'Aarif', age: 27 },
+    { id: 3, name: 'Kavin', age: 30 }
+  ]
+
+  return (
+    <div>
+      <h1>Friends</h1>
+      <ul>
+        {
+          friends.map(friend => 
+            <li key={index}>{friend.name} {friend.age}</li>
+          )
+        }
+      </ul>
+    </div>
+  )
+}
